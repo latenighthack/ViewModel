1b5c119104d39efbda0ee1797b3a5b8d8934c735
Mike Roberts <mike@roll.gg>
2024-08-01 19:05:00 -0400

    Rename swiftpackage at root (ViewModelSupport -> ViewModel)

--- Package.swift
+++ Package.swift
@@ -4,15 +4,15 @@
 import PackageDescription
 
 let package = Package(
-    name: "ViewModelSupport",
+    name: "ViewModel",
     platforms: [
         .iOS(.v13)
     ],
     products: [
         // Products define the executables and libraries a package produces, making them visible to other packages.
         .library(
-            name: "ViewModelSupport",
-            targets: ["ViewModelSupport"]),
+            name: "ViewModel",
+            targets: ["ViewModel"]),
         
     ],
     dependencies: [
@@ -22,12 +22,12 @@ let package = Package(
         // Targets are the basic building blocks of a package, defining a module or a test suite.
         // Targets can depend on other targets in this package and products from dependencies.
         .target(
-            name: "ViewModelSupport",
+            name: "ViewModel",
             dependencies: [
                 .product(name: "ViewModelCore", package: "ViewModelCore")
             ]),
         .testTarget(
-            name: "ViewModelSupportTests",
-            dependencies: ["ViewModelSupport"]),
+            name: "ViewModelTests",
+            dependencies: ["ViewModel"]),
     ]
 )
